b01e7ad6da2ececd91336c55306b057a1533509e
Bruno Araujo <baraujo1105@gmail.com>
2021-04-30 10:45:03 -0300

    Calculadora de Soma!

--- ex003.py
+++ ex003.py
@@ -1,4 +1,4 @@
 print ('Some números')
 número = int(input('Coloque o primeiro número -  '))
 número2 = int(input('Coloque o segundo número -  '))
-print (f'A soma dos números é: {Número+Número2}'')
+print (f'A soma dos números é: {número+número2}')
